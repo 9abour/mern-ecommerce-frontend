fe5f0118378164abd08464b2522a7c3422391803
9abour <9abour@gmail.com>
2023-09-22 09:41:33 +0300

    Convert the helper class to container of helpers to avoid error when trying to use hooks.

--- src/components/navbar/helpers/navbar.helper.ts
+++ src/components/navbar/helpers/navbar.helper.ts
@@ -1,19 +1,17 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 
-export class NavbarHelper {
-	public static getIsWindowScrolled(): boolean {
-		const [isScrolled, setIsScrolled] = useState(false);
+export const getIsWindowScrolled = (): boolean => {
+	const [isScrolled, setIsScrolled] = useState(false);
 
-		useEffect(() => {
-			window.addEventListener("scroll", () => {
-				setIsScrolled(window.pageYOffset > 1 ? true : false);
-			});
-		}, []);
-
-		const isScrolledValue = useMemo(() => {
-			return isScrolled;
-		}, [isScrolled]);
-
-		return isScrolledValue;
+	if (typeof window !== undefined) {
+		window.addEventListener("scroll", () => {
+			setIsScrolled(window.pageYOffset > 1 ? true : false);
+		});
 	}
-}
+
+	const isScrolledValue = useMemo(() => {
+		return isScrolled;
+	}, [isScrolled]);
+
+	return isScrolledValue;
+};
