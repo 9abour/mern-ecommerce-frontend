aed71d90d4ca1d9db7ae0fa60ac19243f776a32e
9abour <9abour@gmail.com>
2023-10-14 15:16:35 +0300

    Enhance performance

--- src/components/aside/components/Menu.tsx
+++ src/components/aside/components/Menu.tsx
@@ -3,12 +3,10 @@ import List from "./List";
 
 const Menu = () => {
 	return (
-		<div>
-			<li>
-				<h5 className="ml-4 text-white uppercase font-semibold">Menu</h5>
-			</li>
+		<>
+			<h5 className="ml-4 text-white uppercase font-semibold">Menu</h5>
 			<List />
-		</div>
+		</>
 	);
 };
 
