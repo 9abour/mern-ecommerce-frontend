463790850996cc98cc70930c0d9d4086a1c5794d
9abour <9abour@gmail.com>
2023-10-14 15:24:08 +0300

    Enhance performance

--- src/components/common/button/MenuButton.tsx
+++ src/components/common/button/MenuButton.tsx
@@ -13,6 +13,7 @@ const MenuButton = ({
 			className={`relative flex md:!hidden w-[60px] min-w-[60px] h-[50px] items-center gap-1 bg-dark py-2 px-1 rounded-l-full ${customStyles}`}
 		>
 			<button
+				aria-label="menu"
 				onClick={() => setIsMenuOpen(!isMenuOpen)}
 				className="w-[40px] min-w-[40px] h-[40px] min-h-[40px] py-[12px] flex flex-col justify-between items-center rounded-full bg-secondaryDark [&>span]:w-[22px] [&>span]:h-[2px] [&>span]:rounded-full [&>span]:bg-white"
 			>
