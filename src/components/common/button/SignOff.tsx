22f23f79b266d5456c3da337ec6fc179fe4a5cfe
9abour <9abour@gmail.com>
2023-10-17 18:36:12 +0300

    Implement SignOff route.

--- src/components/common/button/SignOff.tsx
+++ src/components/common/button/SignOff.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import React from "react";
 import TextButton from "./TextButton";
+import axios from "axios";
+import { toast } from "sonner";
 
 const SignOff = () => {
+	const signOff = async () => {
+		try {
+			await axios.post("api/auth/signoff");
+			window.location.reload();
+			toast.success("Signed Off");
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
 	return (
 		<TextButton
 			text="Sign Off"
 			customStyles="block mx-auto uppercase text-sm bg-secondaryDark text-gray-200 hover:shadow-md hover:bg-white hover:text-dark transition duration-500"
+			onclick={() => signOff()}
 		/>
 	);
 };
